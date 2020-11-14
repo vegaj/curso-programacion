c7ec17af4247986eb6d24411a0e362310a7aa77e
Jose Miguel Vega <jmivega@theworkshop.com>
2020-11-14 21:37:06 +0100

    First commit for basic-python/simplesearch

---
+++ basic-python/simplesearch.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+# Return the smallest element within elements
+def get_smallest(elements):
+    pass
+
+
+# Return the index where the smallest element within elements is
+def find_smallest(elements):
+    pass
+
+
+# Return the position of the first occurrence of target within elements
+def find_target(elements, target):
+    pass
+
+
+# Return a list of indexes where target is within elements.
+# The indexes must be in natural order
+def find_target_positions(elements, target):
+    pass
+
+
+# Return a list containing the :result_length smallest elements within elements.
+# the result list must be in natural order
+def get_smallest_list(elements, result_length):
+    pass
+
+#########
+# TESTS #
+#########
+
+class TestGetSmallest:
+
+    def test_empty(self):
